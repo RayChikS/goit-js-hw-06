7f5e03ff4a7f265943bb54215001ed611b04285c
RayChikS <vatoresp@gmail.com>
2023-10-18 15:50:53 +0300

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,4 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
+const ingredientList = document.querySelector('#ingredients');
+	
+const ingredientItems = ingredients.map(ingredient => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.className = "item";
+  return li;
+});
 
+ingredientList.append(...ingredientItems);
