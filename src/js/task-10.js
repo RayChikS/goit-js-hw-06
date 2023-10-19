feaebc2b64688bdd37f59da5815697162fac5d05
RayChikS <vatoresp@gmail.com>
2023-10-19 14:50:40 +0300

    task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.append(box);
+
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
+
+;
